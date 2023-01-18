2957da23d7b86c11b9e3eba02d4ccb869a3b224d
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-18 14:31:53 -0300

    feat: implementa a função scrape_next_page_link, que é responsável por fazer o scrape do link da próxima página

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,10 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    next_page_url = selector.css(".next::attr(href)").get()
+    return next_page_url
 
 
 # Requisito 4
