5b750f1dadfd5499e835c9d6e99ae9c20eac9359
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-18 19:10:40 -0300

    feat: implementa a função top_5_news, que retorna uma lista de tuplas com as notícias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,26 @@
+from tech_news.database import find_news
+from tech_news.analyzer.search_engine import get_news_tuples
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    sorted_list = sorted(
+        news, key=lambda x: (x["comments_count"], x["title"]), reverse=True
+    )
+
+    if len(sorted_list) <= 5:
+        return get_news_tuples(sorted_list)
+
+    top_five_news_list = []
+
+    for i in range(5):
+        top_five_news_list.append(sorted_list[i])
+
+    return get_news_tuples(top_five_news_list)
+
+
+print(top_5_news())
 
 
 # Requisito 11
