2dacad87bdc8306147995e35da2793fbe628b28b
Rafael Moraes <rafaelsm003@gmail.com>
2023-01-18 19:38:46 -0300

    feat: implementa a função top_5_categories, que retorna uma lista com as categorias mais populares

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,12 +1,15 @@
 from tech_news.database import find_news
 from tech_news.analyzer.search_engine import get_news_tuples
+from collections import Counter
 
 
 # Requisito 10
 def top_5_news():
-    news = find_news()
+    news_list = find_news()
     sorted_list = sorted(
-        news, key=lambda x: (x["comments_count"], x["title"]), reverse=True
+        news_list,
+        key=lambda x: (x["comments_count"], x["title"]),
+        reverse=True,
     )
 
     if len(sorted_list) <= 5:
@@ -20,9 +23,16 @@ def top_5_news():
     return get_news_tuples(top_five_news_list)
 
 
-print(top_5_news())
-
-
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+
+    categories = [news["category"] for news in news_list]
+
+    most_common_categories = Counter(categories).most_common(5)
+
+    sorted_categories = sorted(
+        most_common_categories, key=lambda x: (-x[1], x[0])
+    )
+
+    return [category[0] for category in sorted_categories]
